95a67c99f9d5eb5edd2d6c031e3a2c7d34fd7f38
Helldragger <christopher1223334444@live.fr>
2019-01-12 15:43:11 +0100

    updated build pipeline to clean the dist folder before building

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 pip install --upgrade pyinstaller
 
+RMDIR /S /Q dist
+
 python setup.py sdist bdist_wheel
 
 pyinstaller FFFLaTeX/__init__.py -n FFFLaTeX -F 
--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
+pip install --upgrade pyinstaller
+
+rm -r ./dist/*
+
 python setup.py sdist bdist_wheel
 pyinstaller FFFLaTeX/__init__.py -n FFFLaTeX -F
